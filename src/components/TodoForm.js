d400997f71da982fcc235c53a2f16a00b922a625
szemanuel <81386019+szemanuel@users.noreply.github.com>
2023-05-23 12:08:08 -0300

    2 files modified

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -3,14 +3,23 @@ import React, { useState } from 'react';
 const TodoForm = () => {
   const [value, setValue] = useState('');
 
+  //Necesitamos un evento que capture el estado cuando hacemos submit del form. En form creamos el onSubmit={handleSubmit}
+  const handleSubmit = (e) => {
+    //cuando hacemos submit por default se reload la página para prevenir eso:
+    e.preventDefault();
+    console.log(value);
+  };
+
   return (
-    <form className="">
+    <form className=""
+    onSubmit={handleSubmit}
+    >
       <input
         type="text"
         className=""
         placeholder="Próxima tarea"
         onChange={(e) => setValue(e.target.value)}
-      />
+        />
       <button type="submit" className="">
         Agregar Tarea
       </button>
--- src/components/TodoWrapper.js
+++ src/components/TodoWrapper.js
@@ -1,8 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { TodoForm } from './TodoForm';
+import { v4 as uuidv4 } from "uuid"
+uuidv4();
 
 const TodoWrapper = () => {
-  return <TodoForm />;
+  const [todos, setTodos] = useState([]);
+
+  //Cómo pasamos el valor del TodoForm al TodoWrapper? Con las props. En el componente TodoForm agregamos una prop denominada addTodo
+
+  const addTodo = (todo) => {
+    setTodos([
+      ...todos,
+      { id: uuidv4(), task: todo, completed: false, isEditing: false },
+    ]);
+  };
+
+  return (
+    <div>
+      <TodoForm addTodo={addTodo} />
+    </div>
+  );
 };
 
 export { TodoWrapper };
