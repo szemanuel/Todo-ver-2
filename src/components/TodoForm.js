b51234a1dfd213cd644b8fbe5b40e4df3771275d
szemanuel <81386019+szemanuel@users.noreply.github.com>
2023-05-23 11:43:30 -0300

    1 file modified

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -1,10 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const TodoForm = () => {
+  const [value, setValue] = useState('');
+
   return (
     <form className="">
-      <input type="text" className="" placeholder="Próxima tarea" />
-      <button type="submit" className=""> </button>
+      <input
+        type="text"
+        className=""
+        placeholder="Próxima tarea"
+        onChange={(e) => setValue(e.target.value)}
+      />
+      <button type="submit" className="">
+        Agregar Tarea
+      </button>
     </form>
   );
 };
