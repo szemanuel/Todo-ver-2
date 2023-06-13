baf880ee0d89cb2b17c47a9c1939180f1fd44c2f
szemanuel <81386019+szemanuel@users.noreply.github.com>
2023-06-13 16:16:13 -0300

    1 file modified

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -2,9 +2,7 @@ import React, { useReducer } from 'react';
 
 const Counter = () => {
   //Estado inicial del reducer
-  const initialState = {
-    value: 0,
-  };
+  const initialState = { value: 0}
 
   //función reductora (podría llamarse de cualquier forma) con switch case.El reducer SIEMPRE tiene que devolver algo. Esto lo logro por medio del default.
   const countReducer = (state, action) => {
@@ -16,22 +14,25 @@ const Counter = () => {
       case 'incrementarPor':
         return { ...state, value: state.value + action.payload };
       default:
-        break;
+        return state;
     }
   };
 
   //El hook useReducer es el que une el estado con el reducer.
   //Al useReducer le puedo mandar un :  I) un reducer II) un estado y III) una función. Se espera que la función le traiga el estado que va a tener la función dsp
 
-  const Reductor = () => {
-    //El dispatch es el type, es decir, el string que va a decir “incrementar” o “decrementar” por ej.
-    const [state, dispatch] = useReducer(countReducer, initialState);
-    console.log(state, dispatch, countReducer, initialState);
-    return <div> {state.value} </div>;
-  };
 
+    //El dispatch es el type, es decir, el string que va a decir “incrementar” o “decrementar” por ej.
+const [state, dispatch] = useReducer(countReducer, initialState);
+console.log(state)
+console.log(dispatch)
 return(
- <div> HOLA  </div> 
+  <div>
+ <label> Valor inicial: {state.value} </label>
+   <button type="button" onClick={() => dispatch({type: 'incrementar'}) } > + 1 </button>  
+   <button type="button" onClick={() => dispatch({type: 'decrementar'}) }> - 1 </button>
+   <button type="button">Click Me!</button>
+   </div>
 )
 }
 export { Counter };
