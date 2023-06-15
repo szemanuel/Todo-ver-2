55991d9ae311e57891925e90d3b90e2d8a11cd27
szemanuel <81386019+szemanuel@users.noreply.github.com>
2023-06-15 12:20:49 -0300

    1 file modified

--- src/components/Counter2.js
+++ src/components/Counter2.js
@@ -23,9 +23,22 @@ function counterReducer(state = initialState, action) {
   return state
 }
 
+
+//The current Redux application state lives in an object called the store .
+//The store is created by passing in a reducer, and has a method called getState that returns the current state value:
 const store = configureStore({ reducer: counterReducer })
 console.log(store.getState())
 // {value: 0}
+
+//Dispatch
+//The Redux store has a method called dispatch. The only way to update the state is to call store.dispatch() and pass in an //action object. The store will run its reducer function and save the new state value inside, and we can call getState() to //retrieve the updated value
+
+store.dispatch({ type: 'counter/increment' })
+console.log(store.getState())
+// {value: 1}
+
+//You can think of dispatching actions as "triggering an event" in the application. Something happened, and we want the //////store to know about it. Reducers act like event listeners, and when they hear an action they are interested in, they ///////update the state in response.
+
 
 
 
