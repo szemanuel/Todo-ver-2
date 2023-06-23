d9c40ca4395af8e6231217f5353621b518df1619
szemanuel <81386019+szemanuel@users.noreply.github.com>
2023-06-23 14:47:16 -0300

    2 files modified

--- src/components/counter/Counter.js
+++ src/components/counter/Counter.js
@@ -9,11 +9,15 @@ import {
 } from './counterSlice'
 import styles from './Counter.module.css'
 
-/* we have a function component called Counter, that stores some data in a useState hook. 
+/* We have a function component called Counter, that stores some data in a useState hook. 
  There is a variable called count, but it's not coming from a useState hook.
 While React includes several built-in hooks like useState and useEffect, other libraries can create their own custom hooks that use React's hooks to build custom logic.
 The React-Redux library has a set of custom hooks that allow your React component to interact with a Redux store.
+First, the useSelector hook lets our component extract whatever pieces of data it needs from the Redux store state.
+Earlier, we saw that we can write "selector" functions, which take state as an argument and return some part of the state value.
 Our components can't talk to the Redux store directly, because we're not allowed to import it into component files. But, useSelector takes care of talking to the Redux store behind the scenes for us. If we pass in a selector function, it calls someSelector(store.getState()) for us, and returns the result.
+Similarly, we know that if we had access to a Redux store, we could dispatch actions using action creators, like store.dispatch(increment()). Since we don't have access to the store itself, we need some way to have access to just the dispatch method.
+The useDispatch hook does that for us, and gives us the actual dispatch method from the Redux store
 */
 
 export function Counter() {
